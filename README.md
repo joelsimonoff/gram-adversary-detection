24fa29c77785c38388cf2d7c4597cf629ce5e79c
joelsimonoff <joelsimonoff@gmail.com>
2020-05-11 19:05:02 -0700

    added documentation and new file structure

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
-# gram-adversary-detection
+# Gram Matrix Adversarial Detectors
+
+This repository is dedicated to exploring the use of gram matrices for adversarial detection.
+
+Main Attributions:
+- Gram Matrix Out Of Distribution Detector (good chunk of source code is from this repo): https://github.com/VectorInstitute/gram-ood-detection
+- Gram matrices were made popular via the Neural Style Transfer paper: https://arxiv.org/pdf/1508.06576.pdf
+
+## Summary Of New Work
+
+We used the Vector Institute's paper *(VI detector)* on utilizing gram matrices to detect out of distribution examples, https://arxiv.org/abs/1912.12510, as a jumping off point for an adversarial example detector.
+
+### Out of Distribution (OOD) vs Adversarial
+
+OOD examples are images that the model is not trained to predict on. For example, CIFAR10 images are out of distribution for a CIFAR100 model. Adversarial examples are images with small perturbations designed to fool models. We are using $L_\infty$ adversarial examples which means that $\lVert \text{Img} - \text{(Img + Pertubation)} \rVert_\infty < \epsilon$.
+
+### Minimum Viable Detector
+
+In order to make repurpose the detector in the most effective way possible, we need to see what to remove from the VI detector and what we may need to add. We found that we did not need to take the gram matrices to a specific power and we do not need to look at the gram matrices from every layers. Looking at the matrices from shallower layers (closer to input) seems to yield better adversarial detection accuracy. We found that in some circumstances we gained accuracy by setting `powers` from the VI detector to `powers=[1/2, 1]`; however, the benefit is minimal at best thus we recommend removing the powers variable.
+
+Gram Matrix Calculator VI Detector:
+
+```python
+def G_p(ob, p):
+    temp = ob.detach()
+
+    temp = temp**p
+    temp = temp.reshape(temp.shape[0],temp.shape[1],-1)
+    temp = ((torch.matmul(temp,temp.transpose(dim0=2,dim1=1)))).sum(dim=2)
+    temp = (temp.sign()*torch.abs(temp)**(1/p)).reshape(temp.shape[0],-1)
+
+    return temp
+```
+
+Our Gram Calculator (no powers):
+
+```python
+def G_p(temp):
+    temp = temp.reshape(temp.shape[0],temp.shape[1],-1)
+    temp = ((torch.matmul(temp,temp.transpose(dim0=2,dim1=1)))).sum(dim=2)
+    return temp.reshape(temp.shape[0],-1)
+```
+
+_Note: we found that summing across dim2 seems to accurately represent the total correlation of a feature with the other features._
+
+For this method, we leave in calibration code that calculates the max/min values of the training set for each layer in the model. This calibration step is slow and bulky. In order to remove the calibration step we used 2 techniques (will still need to tune a threshold, but less intensive than the prior detector):
+
+1. **Margin Detector:** Calculating a margin between adversarial perturbed examples and their corresponding test set examples. This works well for training loops because we'll often have both an adversarial example and its corresponding original image. However, this cannot work as a practical detector because at test time we don't have access to non-adversarial perturbed images in addition to the corresponding adversarial examples. The benefit to training in the loop with this detector is that it will force adversarial perturbations to cause an increase to the magnitude of the gram matrices. Unfortunately, it does this by obfuscating the gradient of the PGD margin loss with respect to the input (with each PGD step the attack gets worse which means the adversarial loss is not smooth).
+
+2. **Score Detector:** This detector is based on the original style loss presented in the neural style transfer paper: https://arxiv.org/pdf/1508.06576.pdf:
+
+```python
+def G_p(self, temp):
+    normalizer = torch.prod(torch.tensor(temp.shape[1:]))
+    temp = temp.reshape(temp.shape[0],temp.shape[1],-1)
+    temp = (torch.matmul(temp,temp.transpose(dim0=2,dim1=1))).reshape(temp.shape[0],-1).sum(dim=1)
+
+    return temp/normalizer
+```
+
+We then average take the average value of the gram vector for each layer. At the end we average the value for each layer to assign a score. The score tends to be higher for adversarial examples than for regular examples. **With this method we see an AUROC in the range of 0.94.** The code for this detector can be found in the `ScoreDetector` class in `new-work/model-training/utils/detector.py`.
+
+
+### Adversaries
+
+For each detector (including the original detector that requires calibration), we need an adversary that can create adversarial examples that fool the detector and the model (adversaries that render the detector ineffective). We created these adversaries in `new-work/model-training/utils/attacks.py`. Most take the form:
+
+$\text{min}_{\text{detectability}} \text{max}_{\text{cross_entropy}} \text{cross_entropy} + \text{detectability}$
+
+Essentially: we want to maximize the cross_entropy (encourages the adversarial example to fool the model), and we want to minimize the detectability (making it hard for the detector to spot the example).
+
+### Training
+
+In order for a model to be consider robust it must meet one of the following two scenarios:
+
+1. It is attack, but correctly classifies adversarial examples. This is the motivation behind adversarial training.
+2. The attack must be detected. If the attack is detected, then the system knows not to trust the output of the model.
+
+We attempt to train the model so it improves the detectability of adversarial examples. The goal is that if we make it near impossible to trick the model and the detector then the attack is effectively useless. Thus our training loss is of the form:
+
+$\text{min}_{\text{cross_entropy}} \text{max}_{\text{detectability}} \text{cross_entropy} + \text{detectability}$
+
+This is essentially the inverse of the loss used by the attacker.
+
+_Note: this is not a GAN because the attacker is a fixed algorithm and not trainable, thus we are only training the model._
+
+## Project Structure
+
+### original-gram-ood-detection
+
+This directory contains code from https://github.com/VectorInstitute/gram-ood-detection that is unmodified. This code is included because it can be helpful to look at for reference.
+
+### new-work
+
+This directory contains our work.
+
+**Top Level**
+
+Jupyter notebooks with experimental code. This code was mainly used to conduct ablation tests and general experimentation.
+
+**model-training:**
+
+- `gram_pgd_scratch.ipynb`: used to prototype new adversarial attacks designed to beat the detector.
+- `WRNTraining_Master.ipynb`: training a model that is resistant to attacks that fool the score detector.
+- `WRNTraining_Margin.ipynb`: training a model that is resistant to attack that fools the margin detector.
+- `utils/attacks.py`: contains PGD based attacks with custom loss functions to fool the detector.
+- `utils/detector.py`: contains detector code.
+- `utils/wrn.py`: PyTorch WRN model.
+- `utils/calculate_log.py`: detector metrics (mostly from VectorInstitute code).
+- `benchmark_ckpts`: normally trained wrn CIFAR10 models to use as a benchmark.
+- `checkpoints_margin_01`: checkpoints for wrn model designed to disable attackers from tricking the margin based detector.
+- `checkpoints_margin_01_v2`: similar to `checkpoints_margin_01` except the attacker for this model will only minimize the margin loss to a point and then focus on maximizing cross_entropy.
+- `checkpoints_score_v1`: designed to disable attacker from tricking the score detector.
---
+++ new-work/model-training/utils/detector.py
@@ -0,0 +1,429 @@
+import utils.calculate_log as callog
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.init as init
+import numpy as np
+import math
+from sklearn import metrics
+
+def G_p(temp):
+    temp = temp.reshape(temp.shape[0],temp.shape[1],-1)
+    temp = ((torch.matmul(temp,temp.transpose(dim0=2,dim1=1)))).sum(dim=2)
+    return temp.reshape(temp.shape[0],-1)
+
+def detect(all_test_deviations,all_ood_deviations, test_confs = None, ood_confs=None, verbose=True, normalize=False):
+    if test_confs is not None:
+        test_confs = np.array(test_confs)
+        ood_confs = np.array(ood_confs)
+
+    average_results = {}
+
+    test_deviations = all_test_deviations.sum(axis=1)
+    ood_deviations = all_ood_deviations.sum(axis=1)
+
+    results = callog.compute_metric(-test_deviations,-ood_deviations)
+    for m in results:
+        average_results[m] = average_results.get(m,0)+results[m]
+
+    return average_results
+
+def cpu(ob):
+    for i in range(len(ob)):
+        for j in range(len(ob[i])):
+            ob[i][j] = ob[i][j].cpu()
+    return ob
+
+def cuda(ob):
+    for i in range(len(ob)):
+        for j in range(len(ob[i])):
+            ob[i][j] = ob[i][j].cuda()
+    return ob
+
+
+class ScoreDetector:
+    def __init__(self, mean=True, normalize_gp=True):
+        self.normalize_gp = normalize_gp
+        self.mean = mean
+        None
+
+    def G_p(self, temp):
+        normalizer = torch.prod(torch.tensor(temp.shape[1:]))
+        temp = temp.reshape(temp.shape[0],temp.shape[1],-1)
+        temp = (torch.matmul(temp,temp.transpose(dim0=2,dim1=1))).reshape(temp.shape[0],-1).sum(dim=1)
+
+        return temp/normalizer
+
+    def get_deviations(self, feat_list):
+        batch_deviations = []
+        for L,feat_L in enumerate(feat_list):
+            dev = 0
+
+            g_p = self.G_p(feat_L)
+            g_p = g_p
+            dev = g_p
+
+            if self.mean:
+                batch_deviations.append(dev.mean())
+            else:
+                batch_deviations.append(dev)
+
+        return batch_deviations
+
+    def score(self, feats):
+        devs = self.get_deviations(feats)
+
+        return torch.stack(devs).mean(dim=0).reshape(-1,)
+
+    def calc_auroc(self, feats_reg, feats_adv):
+        labels = np.array([0] * len(feats_reg[0]) + [1] * len(feats_adv[0]))
+
+        old_mean = self.mean
+
+        self.mean = False
+        scores = np.concatenate((self.score(feats_reg).cpu(), self.score(feats_adv).cpu()))
+        self.mean = old_mean
+
+        fpr, tpr, _ = metrics.roc_curve(labels, scores)
+
+        return metrics.auc(fpr, tpr)
+
+
+class Detector:
+    def __init__(self, model, data_train, data_test=None, batch_size=128, pbar = None, batch_detector = False, train_golds=None):
+        if pbar is None:
+            self.pbar = lambda x, total = None: x
+            self.verbose = False
+        else:
+            self.pbar = pbar
+            self.verbose = True
+        self.all_test_deviations = None
+        self.target_deviation = None
+        self.mins = {}
+        self.maxs = {}
+        self.batch_size = batch_size
+        self.classes = range(10)
+        self.torch_model = model
+        self.batch_detector = batch_detector
+
+        if self.batch_detector:
+            self.train_preds = train_golds
+
+            self.compute_batch_minmaxs(data_train)
+        else:
+            self.train_preds = []
+            self.train_confs = []
+            self.train_logits = []
+            for idx in range(0,len(data_train),batch_size):
+                batch = torch.squeeze(torch.stack([x[0] for x in data_train[idx:idx+batch_size]]),dim=1).cuda()
+
+                logits = self.torch_model(batch)
+                confs = F.softmax(logits,dim=1).cpu().detach().numpy()
+                preds = np.argmax(confs,axis=1)
+                logits = (logits.cpu().detach().numpy())
+
+                self.train_confs.extend(np.max(confs,axis=1))
+                self.train_preds.extend(preds)
+                self.train_logits.extend(logits)
+
+            self.test_preds = []
+            self.test_confs = []
+            self.test_logits = []
+            for idx in range(0,len(data_test),batch_size):
+                batch = torch.squeeze(torch.stack([x[0] for x in data_test[idx:idx+batch_size]]),dim=1).cuda()
+
+                logits = self.torch_model(batch)
+                confs = F.softmax(logits,dim=1).cpu().detach().numpy()
+                preds = np.argmax(confs,axis=1)
+                logits = (logits.cpu().detach().numpy())#**2)#.sum(axis=1)
+
+                self.test_confs.extend(np.max(confs,axis=1))
+                self.test_preds.extend(preds)
+                self.test_logits.extend(logits)
+
+            if self.verbose:
+                print("Computing Train Set Min/Maxs:")
+            self.compute_minmaxs(data_train)
+            if self.verbose:
+                print("Computing Test Set Deviations:")
+            self.compute_test_deviations(data_test)
+
+    def compute_batch_minmaxs(self, batch, POWERS=[1]):
+        for PRED in self.pbar(self.classes):
+            train_indices = np.where(np.array(self.train_preds.cpu())==PRED)[0]
+            if len(train_indices) > 0:
+                train_PRED = torch.squeeze(torch.stack([batch[i] for i in train_indices]),dim=1)
+            else:
+                train_PRED = []
+
+            mins,maxs = self.torch_model.get_min_max(train_PRED,power=POWERS)
+            self.mins[PRED] = mins
+            self.maxs[PRED] = maxs
+            torch.cuda.empty_cache()
+
+    def compute_minmaxs(self,data_train,POWERS=[1]):
+        for PRED in self.pbar(self.classes):
+            train_indices = np.where(np.array(self.train_preds)==PRED)[0]
+            if len(train_indices) > 0:
+                train_PRED = torch.squeeze(torch.stack([data_train[i][0] for i in train_indices]),dim=1)
+            else:
+                train_PRED = []
+
+            mins,maxs = self.torch_model.get_min_max(train_PRED,power=POWERS)
+            self.mins[PRED] = mins
+            self.maxs[PRED] = maxs
+            torch.cuda.empty_cache()
+
+    def compute_test_deviations(self,data_test,POWERS=[1]):
+        all_test_deviations = None
+        all_test_deviations_msp = None
+        for PRED in self.pbar(self.classes):
+            test_indices = np.where(np.array(self.test_preds)==PRED)[0]
+            if len(test_indices) > 0:
+                test_PRED = torch.squeeze(torch.stack([data_test[i][0] for i in test_indices]),dim=1)
+            else:
+                test_PRED = []
+
+            mins = self.mins[PRED]
+            maxs = self.maxs[PRED]
+
+            if len(test_PRED) != 0 and len(mins) != 0 and len(maxs) != 0:
+                test_deviations = self.torch_model.get_deviations(test_PRED,power=POWERS,mins=mins,maxs=maxs)
+
+                if all_test_deviations is None:
+                    all_test_deviations = test_deviations
+                else:
+                    all_test_deviations = np.concatenate([all_test_deviations,test_deviations],axis=0)
+
+            torch.cuda.empty_cache()
+
+        self.all_test_deviations = all_test_deviations
+        self.target_deviation = torch.tensor(all_test_deviations.mean(axis=0).sum() * 0.85)
+
+    def compute_ood_deviations_batch(self,ood,POWERS=[1],msp=False):
+        ood_preds = []
+        ood_confs = []
+
+        for idx in range(0,len(ood),128):
+            batch = torch.squeeze(torch.stack([x for x in ood[idx:idx+128]]),dim=1).cuda()
+            logits = self.torch_model(batch)
+            confs = F.softmax(logits,dim=1).cpu().detach().numpy()
+            preds = np.argmax(confs,axis=1)
+
+            ood_confs.extend(np.max(confs,axis=1))
+            ood_preds.extend(preds)
+            torch.cuda.empty_cache()
+
+        all_ood_deviations = None
+        all_ood_deviations_MSP = None
+        all_ood_confs = []
+
+        for PRED in self.classes:
+            ood_indices = np.where(np.array(ood_preds)==PRED)[0]
+            if len(ood_indices)==0:
+                continue
+
+            ood_PRED = torch.squeeze(torch.stack([ood[i] for i in ood_indices]),dim=1)
+
+            ood_confs_PRED =  np.array([ood_confs[i] for i in ood_indices])
+
+            all_ood_confs.extend(ood_confs_PRED)
+
+            mins = self.mins[PRED]
+            maxs = self.maxs[PRED]
+            ood_deviations = self.torch_model.get_deviations(ood_PRED,power=POWERS,mins=mins,maxs=maxs)
+            ood_deviations_MSP = ood_deviations/ood_confs_PRED[:,np.newaxis]
+
+            if all_ood_deviations is None:
+                all_ood_deviations = ood_deviations
+                all_ood_deviations_MSP = ood_deviations_MSP
+            else:
+                all_ood_deviations = np.concatenate([all_ood_deviations,ood_deviations],axis=0)
+                all_ood_deviations_MSP = np.concatenate([all_ood_deviations_MSP,ood_deviations_MSP],axis=0)
+            torch.cuda.empty_cache()
+
+        self.all_ood_confs = all_ood_confs
+
+        average_results = detect(self.all_test_deviations,all_ood_deviations)
+
+        return average_results
+
+    def get_deviation(self, feat_list, idx, mins, maxs):
+        batch_deviations = []
+        for L,feat_L in enumerate(feat_list):
+            dev = 0
+
+            g_p = G_p(feat_L)[idx]
+
+            dev +=  (F.relu(mins[L][0]-g_p)/torch.abs(mins[L][0]+10**-6)).sum(dim=1,keepdim=True)
+            dev +=  (F.relu(g_p-maxs[L][0])/torch.abs(maxs[L][0]+10**-6)).sum(dim=1,keepdim=True)
+
+            batch_deviations.append(dev)
+
+        return batch_deviations
+
+    def gram_loss(self, logits, feats):
+        if not self.batch_detector:
+            assert self.target_deviation is not None
+
+        confs = F.softmax(logits, dim=1)
+
+        _, indices = torch.max(confs, 1)
+
+        loss = 0
+        for i in range(10):
+            idxs = indices == i
+
+            if idxs.sum() == 0 or len(self.mins[i]) == 0 or len(self.maxs[i]) == 0:
+                continue
+
+            batch_dev = self.get_deviation(feats, idxs, mins=self.mins[i], maxs=self.maxs[i])
+            batch_dev = torch.squeeze(torch.stack(batch_dev, dim=1))
+
+            loss += batch_dev.sum()/logits.shape[0]
+
+        if self.batch_detector:
+            return F.relu(loss/(logits.shape[0] + 10**-6))
+        else:
+            return F.relu(loss - self.target_deviation + 10)
+
+    def compute_auroc_advs(self, adv_logits, adv_feats, adv_ys):
+        confs = F.softmax(adv_logits,dim=1).cpu().numpy()
+        preds = np.argmax(confs,axis=1)
+
+        adv_deviations = None
+        failed_adv_deviations = None
+
+        for PRED in self.classes:
+            idxs = np.where((np.array(preds) == PRED) & (np.array(adv_ys) != PRED))[0]
+            idxs_failed = np.where((np.array(preds) == PRED) & (np.array(adv_ys) == PRED))[0]
+
+            mins = self.mins[PRED]
+            maxs = self.maxs[PRED]
+
+            adv_dev_class = get_deviations(select_features(adv_feats,idxs), mins=mins, maxs=maxs)
+            failed_adv_dev_class = get_deviations(select_features(adv_feats,idxs_failed), mins=mins, maxs=maxs)
+
+            if len(adv_dev_class) > 0:
+                if adv_deviations is None:
+                    adv_deviations = adv_dev_class
+                else:
+                    adv_deviations = np.concatenate([adv_deviations, adv_dev_class],axis=0)
+
+            if len(failed_adv_dev_class) > 0:
+                if failed_adv_deviations is None:
+                    failed_adv_deviations = failed_adv_dev_class
+                else:
+                    failed_adv_deviations = np.concatenate([failed_adv_deviations, failed_adv_dev_class], axis=0)
+
+        failed_results, results = 0,0
+        if failed_adv_deviations is not None and len(failed_adv_deviations) != 0:
+            failed_results = detect(self.all_test_deviations, failed_adv_deviations)["AUROC"]
+
+        if adv_deviations is not None and len(adv_deviations) != 0:
+            results = detect(self.all_test_deviations, adv_deviations)["AUROC"]
+
+        return results, failed_results
+
+def gram_margin_loss(feats_reg, feats_adv, margin):
+    assert len(feats_reg) == len(feats_adv)
+
+    layer_deviations = torch.zeros((len(feats_reg), len(feats_reg[0])))
+    for i in range(len(feats_reg)):
+        g_p_reg = G_p(feats_reg[i])
+        g_p_adv = G_p(feats_adv[i])
+
+        clamp = torch.tensor(1.0).cuda()
+
+        orig_max = torch.max(g_p_reg, dim=1)[0]
+        clamped_max = torch.max(torch.abs(orig_max), clamp)
+
+        max_dist = F.relu(torch.max(g_p_adv, dim=1)[0] - orig_max)/clamped_max
+
+        orig_min = torch.min(g_p_reg, dim=1)[0]
+        clamped_min = torch.max(torch.abs(orig_min), clamp)
+
+        min_dist = F.relu(orig_min - torch.min(g_p_adv, dim=1)[0])/clamped_min
+
+        layer_deviations[i] = max_dist + min_dist
+
+    return F.relu(margin - layer_deviations.mean(dim=0)).pow(2).mean()
+
+def feats_idxs(feats, idxs):
+    return [f[idxs] for f in feats]
+
+def new_gram_margin_loss(feats_reg, output_reg, feats_adv, output_adv, golds, margin=20):
+    indices_reg = torch.max(output_reg.cpu(), 1)[1]
+    indices_adv = torch.max(output_adv.cpu(), 1)[1]
+
+    total_size = 0
+    loss = []
+    for i in range(10):
+        idxs_adv = torch.where(i == indices_adv)[0]
+        idxs_reg = torch.where(i == golds)[0]
+
+        batch_size = min(idxs_adv.shape[0], idxs_reg.shape[0])
+        if batch_size == 0:
+            continue
+
+        if batch_size < idxs_adv.shape[0]:
+            idxs_reg = torch.cat([idxs_reg] * 10)
+            batch_size = idxs_adv.shape[0]
+            if idxs_reg.shape[0] < batch_size:
+                continue
+
+            idxs_reg = idxs_reg[:batch_size]
+        total_size += batch_size
+
+        idxs_adv, idxs_reg = idxs_adv[:batch_size], idxs_reg[:batch_size]
+
+        class_loss = gram_margin_loss(feats_idxs(feats_reg, idxs_reg), feats_idxs(feats_adv, idxs_adv), margin)
+        loss.append(class_loss)
+
+    return torch.stack(loss).sum()
+
+def calc_auroc(all_test_deviations,all_ood_deviations):
+    average_results = {}
+
+    test_deviations = all_test_deviations.sum(axis=1)
+    ood_deviations = all_ood_deviations.sum(axis=1)
+
+    results = callog.compute_metric(-test_deviations,-ood_deviations)
+
+    return results["AUROC"]
+
+def select_features(feat_list, idxs):
+    return [f[idxs] for f in feat_list]
+
+def get_deviations(feat_list, mins,maxs):
+    if len(mins) == 0 or len(maxs) == 0 or len(feat_list) == 0 or len(feat_list[0]) == 0:
+        return np.array([])
+
+    deviations = []
+    for L,feat_L in enumerate(feat_list):
+
+        g_p = G_p(feat_L)
+
+        dev =  (F.relu(mins[L][0]-g_p)/torch.abs(mins[L][0]+10**-6)).sum(dim=1,keepdim=True)
+        dev +=  (F.relu(g_p-maxs[L][0])/torch.abs(maxs[L][0]+10**-6)).sum(dim=1,keepdim=True)
+
+        deviations.append(dev.cpu().numpy())
+
+    deviations = np.concatenate(deviations, axis=1)
+
+    return deviations
+
+def gram_matrix(layer):
+    b, ch, h, w = layer.size()
+    features = layer.view(b, ch, w * h)
+    gram = torch.matmul(features, features.transpose(1, 2))
+
+    return gram /(ch * h * w)
+
+def style_loss(lhs, rhs):
+    loss = 0.0
+    for i in range(len(lhs)):
+        loss += (gram_matrix(lhs[i]) - gram_matrix(rhs[i])).pow(2).sum()
+
+    return loss
